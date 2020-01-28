b0e38b92a970ef150467fdf62f08fcf27f02b0b5
baginski95 <bagiczny3@gmail.com>
2020-01-28 11:13:44 +0100

    Refactored getting albums from file in main_program logic

--- main_program.py
+++ main_program.py
@@ -7,8 +7,7 @@ import display
 import sys
 
 MAIN_MENU = ['Add album', 'Remove Album', 'Get albums by genre', 'Get longest album', 'Get total albums length', 'Get genres stats', 'Get oldest album', 'Exit']
-GENRE_NAME = 0
-AMOUNT_OF_ALBUMS = 1
+ALBUM_NAME = 0
 
 
 def get_inputs(list_labels):
@@ -19,16 +18,13 @@ def get_inputs(list_labels):
     return inputs
 
 
-def add_album():
-    albums = file_handling.import_data()
+def add_album(albums):
     album_properties = get_inputs(['artist name: ', 'album name: ', 'release year: ', 'genre: ', 'length: '])
     albums.append(album_properties)
     file_handling.export_data(albums, mode='w')
 
 
-def remove_album():
-    ALBUM_NAME = 0
-    albums = file_handling.import_data()
+def remove_album(albums):
     get_album_name = get_inputs(['album name: '])
     for album in albums[:]:
         if album[ALBUM_NAME] == get_album_name[0]:
@@ -48,31 +44,26 @@ def main():
     is_program_working = True
     while is_program_working:
         display.print_program_menu(MAIN_MENU)
-
+        albums = file_handling.import_data()
         choose_option = get_inputs(['Enter option number: '])
         option = choose_option[0]
         if option == '0':
-            add_album()
+            add_album(albums)
         elif option == '1':
-            remove_album()
+            remove_album(albums)
         elif option == '2':
             get_genre = get_inputs(['genre: '])
-            albums = file_handling.import_data()
             display.print_albums_list(music_reports.get_albums_by_genre(albums, get_genre[0]))
         elif option == '3':
-            albums = file_handling.import_data()
             display.print_album_info(music_reports.get_longest_album(albums))
         elif option == '4':
-            albums = file_handling.import_data()
             total_albums_length = music_reports.get_total_albums_length(albums)
             display.print_command_result(f'Total albums length in mins: {total_albums_length}')
         elif option == '5':
-            albums = file_handling.import_data()
             genre_stats = music_reports.get_genre_stats(albums)
             for genre_name, amount_of_albums in genre_stats.items():
                 display.print_command_result(f'{genre_name}|{amount_of_albums}')
         elif option == '6':
-            albums = file_handling.import_data()
             oldest_album = music_reports.get_oldest_album(albums)
             display.print_album_info(oldest_album)
         elif option == '7':
